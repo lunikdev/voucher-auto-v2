9b6b4b1e1ab77d36834b37e5a264487fff7f68de
Leandro Gonçalves <119635870+lunikdev@users.noreply.github.com>
2025-05-09 10:17:22 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Sistema de Vouchers',
-  description: 'Sistema para validação de vouchers',
+  title: 'Sistema de Login Único',
+  description: 'Sistema para validação de login único',
 };
 
 export default function RootLayout({
